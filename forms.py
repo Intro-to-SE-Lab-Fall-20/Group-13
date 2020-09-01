76e8c04934ae468b8e74eff74f61651eb574a660
CWGU <wickedcoastal@limepi.com>
2020-08-31 20:43:56 -0500

    Added new nYlas functionality with Emails load, simulating search and compose functions

---
+++ forms.py
@@ -0,0 +1,11 @@
+#This file stores forms for the application.
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Length, EqualTo, Email
+
+class LoginForm(FlaskForm):
+    email = StringField('email', validators=[DataRequired(),Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
