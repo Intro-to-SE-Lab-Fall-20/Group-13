c3ea6d94e39c698868cfb6092c52e6b0d8735f82
CWGU <wickedcoastal@limepi.com>
2020-08-31 21:27:47 -0500

    Updated app.py email-search route for testing

--- app.py
+++ app.py
@@ -30,9 +30,7 @@ def default1():
 @app.route("/email/", methods=['GET', 'POST'])
 def email():
     from nylas import APIClient
-    CLIENT_ID='erta7s4vw61q37aw14wx1f7kv'
-    ACCESS_TOKEN='GD7S4grE0FruLFBm2TiixeyLY3YoGC'
-    CLIENT_SECRET='1coubzlaae6c0irdoyya36qby'
+  
     nylas = APIClient(    CLIENT_ID,
     CLIENT_SECRET,
     ACCESS_TOKEN    
@@ -49,9 +47,7 @@ def email():
 @app.route("/email-search/", methods=['GET', 'POST'])
 def emailsearch():
     from nylas import APIClient
-    CLIENT_ID='erta7s4vw61q37aw14wx1f7kv'
-    ACCESS_TOKEN='GD7S4grE0FruLFBm2TiixeyLY3YoGC'
-    CLIENT_SECRET='1coubzlaae6c0irdoyya36qby'
+
     nylas = APIClient(    CLIENT_ID,
     CLIENT_SECRET,
     ACCESS_TOKEN    
